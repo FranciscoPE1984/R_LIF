3069006b9b7d9b33c5a1576754898959a2d25128
FranciscoPE1984 <123676214+FranciscoPE1984@users.noreply.github.com>
2024-08-06 15:46:23 -0300

    Update app.py

--- app.py
+++ app.py
@@ -327,8 +327,7 @@ def relatorio_admin():
     return redirect(url_for('login'))
 
 
-
-
-
 if __name__ == '__main__':
-    app.run(debug=True)
+    #app.run(debug=True)
+    # Configuração para rodar o Flask em todas as interfaces de rede e na porta 80
+    app.run(host='0.0.0.0', port=80, debug=True)
